88e70fe04aa667746a3daf99872c6841d00d31cc
abinaya.it19 <abinayak.it19@bitsathy.ac.in>
2019-10-04 11:44:17 +0530

    first commit

---
+++ input.py
@@ -0,0 +1,12 @@
+x=input("enter a")
+y=input("enter b")
+c=x+y
+d=x-y
+e=x*y
+f=x/y
+print (c)
+print (d)
+print (e)
+print (f)
+
+
---
+++ star.c
@@ -0,0 +1,23 @@
+#include <stdio.h>  
+   
+int main() {  
+    int i, j, rows;  
+   
+    printf("Enter the number of rows\n");
+    scanf("%d", &rows); 
+   
+    for(i = 1; i <= rows; i++){  
+    
+        for(j = 1; j <= rows; j++){
+            if(j <= rows-i){
+                printf(" "); 
+            } else {
+                printf("*"); 
+       }
+        }
+        
+        printf("\n");  
+    }  
+       
+    return 0;  
+} 
